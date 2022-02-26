0171b58be81a1c4f9a7d43734f11d8ec1f71c7ca
Lyuba Georgieva <lyubageorgieva@hotmail.com>
2022-02-26 17:58:49 -0500

    Creation of Sign In Screen

--- src/screens/SignInScreen.js
+++ src/screens/SignInScreen.js
@@ -1,17 +1,76 @@
-import { StyleSheet, Text, View } from 'react-native';
-import React from 'react';
+import { StyleSheet, View, Image, TouchableOpacity} from 'react-native';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
+import React, { useState } from 'react';
+import { Input, Text, Button} from '@ui-kitten/components';
 
-const SignInScreen = () => {
+
+const SignInScreen = ({ navigation }) => {
+
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('');
+    const [secureTextEntry, setSecureTextEntry] = React.useState(true);
+
     return (
-        <View>
-            <Text></Text>
-            <Text>Sign In screen</Text>
-        </View>
+        <KeyboardAwareScrollView>
+            <Image style={styles.header} source={require('../../assets/ChicagoPic2.jpg')}/>
+            <KeyboardAwareScrollView resetScrollToCoords={{ x: 0, y: 0 }} scrollEnabled={false} >
+                <View style={styles.content}>
+                    <Text style={styles.primaryText}  category='h5'>LOGIN</Text>
+                    <Input style={styles.inputContainer} status='basic' autoCapitalize='none' placeholder='Username' onChangeText={username => setUsername(username)} />
+                    <Input style={styles.inputContainer} status='basic' autoCapitalize='none' placeholder='Password' secureTextEntry={secureTextEntry} onChangeText={password => setPassword(password)} />
+                    <View style={styles.buttonBox}>
+                        <Button style={styles.buttonColors}>LOGIN</Button>
+                    </View>
+                </View>
+            </KeyboardAwareScrollView>
+        </KeyboardAwareScrollView>
     );
 };
 
 export default SignInScreen;
 
 const styles = StyleSheet.create({
-    
+    content: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingTop: 20,
+    },
+    header: {
+        height: 250, 
+        width: '100%',
+    },
+    primaryText: {
+        fontFamily: "Verdana",
+        paddingTop: 20,
+        paddingBottom: 30,
+    },
+    secondaryText: {
+        fontFamily: "Verdana",
+        paddingTop: 60,
+        paddingBottom: 10,
+    },
+    inputContainer: {
+        margin: 15,
+        borderRadius: 6,
+        marginTop: 10,
+        marginBottom: 10,
+        marginLeft: 30,
+        marginRight: 30
+    },
+    buttonColors: {
+        backgroundColor: "#FFA78C",
+        borderColor: 'transparent',
+    },
+    buttonBox: {
+        paddingTop: 30,
+    },
+    signInButton: {
+        fontFamily: "Verdana",
+        backgroundColor: "transparent",
+        borderColor: 'transparent',
+    },
+    linkColor: {
+        fontFamily: "Verdana",
+        color: "#FF9180",
+    },
 });
