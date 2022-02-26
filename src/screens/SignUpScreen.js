c7712ee9e10101fc0bfd8cb5e6e9cd4fac9d23e3
Lyuba Georgieva <lyubageorgieva@hotmail.com>
2022-02-26 15:12:02 -0500

    Creation of the Sign Up Screen

--- src/screens/SignUpScreen.js
+++ src/screens/SignUpScreen.js
@@ -1,11 +1,20 @@
-import { StyleSheet, Text, View } from 'react-native';
-import React from 'react';
+import { StyleSheet, Text, View, Image} from 'react-native';
+import React, { useState } from 'react';
+import { Icon, Input} from '@ui-kitten/components';
+
+const AlertIcon = (props) => (
+    <Icon {...props} name='alert-circle-outline'/>
+);
 
 const SignUpScreen = () => {
+
+    const [value, setValue] = React.useState('');
+
     return (
         <View>
-            <Text></Text>
-            <Text>Sign up screen</Text>
+            <Image style={styles.header} source={require('../../assets/ChicagoPic1.jpg')}/>
+            <Text>Welcome!</Text>
+            <Input placeholder='Place your Text' value={value} onChangeText={nextValue => setValue(nextValue)}/>
         </View>
     );
 };
@@ -13,5 +22,9 @@ const SignUpScreen = () => {
 export default SignUpScreen;
 
 const styles = StyleSheet.create({
-    
+
+    header: {
+    height: 200, 
+    width: 400,
+    },
 });
